3b9a21ad62f09860d5ce990040d7fb2bb170b1e8
revofarissaifuddin <revosaifuddin@gmail.com>
2023-02-27 12:38:12 +0700

    Unggah Gambar (validation png, jpg, file size)

--- src/middleware/uploadPhoto.js
+++ src/middleware/uploadPhoto.js
@@ -1,17 +1,28 @@
 const multer = require('multer')
+const path = require("path");
 
+
+// validate uploaded files
+const FILE_TYPE_MAP = {
+  // mime type
+    "image/png": "png",
+    "image/jpg": "jpg",
+    "image/jpeg": "jpeg",
+};
 const storage = multer.diskStorage({
     destination: function (req, res, cb) {
         cb(null,'./tmp')
     },
     filename: function (req, res, cb) {
+        const extension = FILE_TYPE_MAP;
         const uniq = Date.now() + Math.round(Math.random() * 1E9)
-        cb(null, uniq + ".png");
+        // cb(null, uniq +".png");
+        cb(null,uniq+ extension);
     }
 })
 
 const upload = multer({
-    storage,
+    storage: storage,
     limits: { fileSize: 10 * Math.pow(1024, 4) },
 });
 
