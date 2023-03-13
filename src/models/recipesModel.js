8d152d8cb6534dd4857dd2f49310a2ee2e35742d
revofarissaifuddin <revosaifuddin@gmail.com>
2023-03-13 16:11:13 +0700

    update recipes get id

--- src/models/recipesModel.js
+++ src/models/recipesModel.js
@@ -7,7 +7,7 @@ const getDataRecipes = () => {
 };
 const getRecipesById = (data) => {
     return pool.query(
-        `SELECT * FROM recipes WHERE recipes.deleted_at IS NULL AND id=${data}`
+        `SELECT * FROM recipes WHERE recipes.deleted_at = NULL AND id=${data}`
     );
 };
 const getRecipesByIdUsers = (data) => {
