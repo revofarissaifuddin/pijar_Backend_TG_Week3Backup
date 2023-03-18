86a84ad60a35edb45afa603b0c7a424e829ab7ce
revofarissaifuddin <revosaifuddin@gmail.com>
2023-03-18 13:14:39 +0700

    update

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-/* app.use(function(err, req, res, next) {
+app.use(function(err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get("env") === "development" ? err : {};
@@ -49,7 +49,7 @@ app.use(function(req, res, next) {
     res.status(err.status || 500);
     res.render("error");
     next(err);
-}); */
+});
 /* import xss */
 app.use(xss());
 
