6a24b5a817d0d56c20f7f71047e162641f6311e5
revofarissaifuddin <revosaifuddin@gmail.com>
2023-04-11 20:08:59 +0700

    update controller user

--- src/routes/users.js
+++ src/routes/users.js
@@ -5,7 +5,7 @@ const { protect } = require("../middleware/auth");
 
 router.get("/", protect, getUsers);
 router.post("/", createUsers);
-router.post("/send-otp", sendOTPEmail);
+router.put("/send-otp", sendOTPEmail);
 router.get("/my-users/", protect, findUserByID);
 router.put("/my-users/", protect, updateUsers);
 router.delete("/my-users/", protect, removeDataById);
